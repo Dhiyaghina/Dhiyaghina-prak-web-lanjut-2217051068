0a6f2c6f546fe987ab4e2eee01f158dd2eac5117
Dhiya <dhiyaghinaaaa@gmail.com>
2024-10-04 15:58:12 +0700

    tugas 1

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile</title>
+</head>
+<body>
+    <p>Nama : Dhiya Ghina Hasri</p>
+    <p>Kelas : D</p>
+    <p>NPM : 2217051068</p>
+</body>
+</html>
